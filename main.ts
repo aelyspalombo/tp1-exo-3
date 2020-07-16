f35fb803429d0d2d8febc6db565f170e5561fd0b
aelyspalombo <68046095+aelyspalombo@users.noreply.github.com>
2020-07-16 15:05:54 +1100

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,65 @@
+let x = 0
+let y = 0
+let a = 0
+let b = 0
+let i = 0
+let j = 0
+basic.forever(function () {
+    x = randint(0, 4)
+    y = 0
+    led.plot(x, y)
+    while (y != 4) {
+        basic.pause(100)
+        led.plotBrightness(x, y, 175)
+        y += 1
+        led.unplot(x, y - 3)
+        led.plot(x, y)
+        led.plotBrightness(x, y - 2, 60)
+    }
+    basic.pause(100)
+    led.unplot(x, y - 2)
+    basic.pause(100)
+    led.unplot(x, y - 1)
+    basic.pause(100)
+    led.unplot(x, y)
+})
+basic.forever(function () {
+    basic.pause(randint(100, 500))
+    a = randint(0, 4)
+    b = 0
+    led.plot(a, b)
+    while (b != 4) {
+        basic.pause(100)
+        led.plotBrightness(a, b, 175)
+        b += 1
+        led.unplot(a, b - 3)
+        led.plot(a, b)
+        led.plotBrightness(a, b - 2, 60)
+    }
+    basic.pause(100)
+    led.unplot(a, b - 2)
+    basic.pause(100)
+    led.unplot(a, b - 1)
+    basic.pause(100)
+    led.unplot(a, b)
+})
+basic.forever(function () {
+    basic.pause(randint(20, 50))
+    i = randint(0, 4)
+    j = 0
+    led.plot(i, j)
+    while (j != 4) {
+        basic.pause(100)
+        led.plotBrightness(i, j, 175)
+        j += 1
+        led.unplot(i, j - 3)
+        led.plot(i, j)
+        led.plotBrightness(i, j - 2, 60)
+    }
+    basic.pause(100)
+    led.unplot(i, j - 2)
+    basic.pause(100)
+    led.unplot(i, j - 1)
+    basic.pause(100)
+    led.unplot(i, j)
+})
